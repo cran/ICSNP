c8d50c51a9e3c4c0686bf28609f794d46f725311
Klaus Nordhausen <klaus.nordhausen@uta.fi>
2010-01-12 00:00:00 +0000

    version 1.0-7

--- R/HP.loc.test.R
+++ R/HP.loc.test.R
@@ -28,55 +28,53 @@ HP.loc.test <- function(X, mu = NULL, score = "rank", angles="tyler", method = "
     
     }
 
-HP.loc.tyler.test <- function(X, mu = NULL, score = "rank", method = "approximation", n.perm = 1000)
-    {
+HP.loc.tyler.test <-
+function (X, mu = NULL, score = "rank", method = "approximation", 
+    n.perm = 1000) 
+{
     k <- dim(X)[2]
     TylerH0 <- tyler.shape(X, location = mu)
-    mahaH0 <-  mahalanobis(X, center = mu, cov = TylerH0)
+    mahaH0 <- mahalanobis(X, center = mu, cov = TylerH0)
     ranksH0 <- rank(mahaH0)
     signsH0 <- spatial.sign(X, center = mu, shape = TylerH0)
-    n<- length(ranksH0)
-    scoresH0 <-   switch(score, sign = 1, rank = ranksH0, normal = sqrt(qchisq(ranksH0/(n+1), k)))
+    n <- length(ranksH0)
+    scoresH0 <- switch(score, sign = 1, rank = ranksH0, normal = sqrt(qchisq(ranksH0/(n + 
+        1), k)))
     signsH0w <- scoresH0 * signsH0
-
-
-    switch(score, sign = {DIV <- k/n
-              METHOD <- "TYLER ANGLES SIGN TEST"
-              S.name <- "Q.S"}
-              , 
-              rank = {DIV <- 3*k / (n * (n+1)^2)
-              METHOD <- "TYLER ANGLES RANK TEST"
-              S.name <- "Q.W"}
-              , 
-              normal = {DIV <-1/n
-              METHOD <- "TYLER ANGLES VAN DER WAERDEN TEST"
-              S.name <- "Q.N"}
-            ) 
-      
-    SUMS <- matrix(0,n,n)
-    for (i in 1:n) SUMS[i,] <- rowSums(sweep(signsH0w, 2, signsH0w[i, ], "*"))
+    switch(score, sign = {
+        DIV <- k/n
+        METHOD <- "TYLER ANGLES SIGN TEST"
+        S.name <- "Q.S"
+    }, rank = {
+        DIV <- 3 * k/(n * (n + 1)^2)
+        METHOD <- "TYLER ANGLES RANK TEST"
+        S.name <- "Q.W"
+    }, normal = {
+        DIV <- 1/n
+        METHOD <- "TYLER ANGLES VAN DER WAERDEN TEST"
+        S.name <- "Q.N"
+    })
+    SUMS <- tcrossprod(signsH0w)
     Q1 <- sum(SUMS)
-
     STATISTIC <- DIV * Q1
-
-
-    switch(method, approximation = {PVAL <-1 - pchisq(STATISTIC, k)
-                                PARAMETER <- k
-                                names(PARAMETER) <- "df"}
-                ,
-                permutation =   {rep.func <- function(index, signs, n)
-                                {
-                                SUMS.2 <- matrix(0,n,n)
-                                for (i in 1:n) SUMS.2[i,] <- rowSums(sweep(signs, 2, signs[i, ], "*"))
-                                sum(SUMS.2)
-                                }
-                             Q.simu <- replicate(n.perm, rep.func(sample(c(1,-1), n, replace = TRUE), signsH0w, n))
-                             PVAL <- mean(Q.simu>Q1)
-                             PARAMETER<-n.perm
-                             names(PARAMETER)<-"permutations"}
-                            )   
-
+    switch(method, approximation = {
+        PVAL <- 1 - pchisq(STATISTIC, k)
+        PARAMETER <- k
+        names(PARAMETER) <- "df"
+    }, permutation = {
+        rep.func <- function(index, signs, n) {
+            signs2 <- index*signs
+            SUMS.2 <- tcrossprod(signs2)
+            sum(SUMS.2)
+        }
+        Q.simu <- replicate(n.perm, rep.func(sample(c(1, -1), 
+            n, replace = TRUE), signsH0w, n))
+        PVAL <- mean(Q.simu > Q1)
+        PARAMETER <- n.perm
+        names(PARAMETER) <- "permutations"
+    })
     names(STATISTIC) <- S.name
-    res <- list(statistic = STATISTIC, p.value = PVAL, method = METHOD, parameter = PARAMETER)
+    res <- list(statistic = STATISTIC, p.value = PVAL, method = METHOD, 
+        parameter = PARAMETER)
     return(res)
-    }
+}
